94af4c6aae53e8506dd5e87da0587b39d0c89102
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-09-15 13:41:47 +0700

    Added app's icon. Added a number of different transform animations.

--- Core Animation Demo/ViewController.swift
+++ Core Animation Demo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.imageView = UIImageView(image: UIImage(named: "penguin"))
+        self.imageView = UIImageView(image: UIImage(named: "Swift"))
         self.imageView.center = CGPointMake(512, 384)
         self.view.addSubview(self.imageView)
     }
@@ -33,9 +33,18 @@ class ViewController: UIViewController {
     @IBAction func tapButtonTapped(sender: UIButton) {
         self.tapButton.hidden = true
         
-        UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions(), animations: { [unowned self] () -> Void in
+        UIView.animateWithDuration(1.0, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 5, options: UIViewAnimationOptions(), animations: { [unowned self] () -> Void in
                 switch self.currentAnimationCounter {
-                    case 0: break
+                    case 0: self.imageView.transform = CGAffineTransformMakeScale(2.0, 2.0)
+                    case 1: self.imageView.transform = CGAffineTransformIdentity
+                    case 2: self.imageView.transform = CGAffineTransformMakeTranslation(-256.0, -256.0)
+                    case 3: self.imageView.transform = CGAffineTransformIdentity
+                    case 4: self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+                    case 5: self.imageView.transform = CGAffineTransformIdentity
+                    case 6:
+                        self.imageView.alpha = 0.1
+                    case 7:
+                        self.imageView.alpha = 1.0
                     default: break
                 }
             }) { [unowned self] (finished: Bool) -> Void in
